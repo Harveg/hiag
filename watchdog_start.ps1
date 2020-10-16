8430f77828050aa9e31171a4c868114fdcf2c920
Harveg <64636733+Harveg@users.noreply.github.com>
2020-10-16 09:31:35 +0200

    Update watchdog_start.ps1
    Inclueded logging feature based on https://thesurlyadmin.com/2015/10/20/transcript-logging-why-you-should-do-it/

--- watchdog_start.ps1
+++ watchdog_start.ps1
@@ -1,4 +1,11 @@
-﻿cls
-"**********HARVEG**********"
+Write-Output "---------------------------------------HARVEG---------------------------------------" 
+$KeepLogsFor = 15
+$VerbosePreference = "Continue"
+$LogPath = Split-Path $MyInvocation.MyCommand.Path
+Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(–$KeepLogsFor) | Remove-Item –Confirm:$false
+$LogPathName = Join-Path –Path $LogPath –ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date –Format 'MM-dd-yyyy').log"
+Start-Transcript $LogPathName –Append
 cd C://Harveg/scripts
 py hiag_watchdog_win
+"------------------------------------------------------------------------" | Out-Default
+Stop-Transcript
