869c6b189ce383aa664328d73cc12eb678ed739b
Harveg <64636733+Harveg@users.noreply.github.com>
2020-10-20 10:31:21 +0200

    Update watchdog_start.ps1
    Changed method for run python script credits to: https://stackoverflow.com/questions/53038933/powershell-stderr-redirect-breaks-lines-every-few-characters

--- watchdog_start.ps1
+++ watchdog_start.ps1
@@ -5,7 +5,8 @@ $LogPath = Split-Path $MyInvocation.MyCommand.Path
 Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(–$KeepLogsFor) | Remove-Item –Confirm:$false
 $LogPathName = Join-Path –Path $LogPath –ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date –Format 'MM-dd-yyyy').log"
 Start-Transcript $LogPathName –Append
-cd C://Harveg/scripts
-py hiag_watchdog_win
 "------------------------------------------------------------------------" | Out-Default
 Stop-Transcript
+cd C://Harveg/scripts
+py -u hiag_watchdog_win | % ToString | Tee-Object $LogPathName
+
